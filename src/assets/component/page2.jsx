bb9f469869c119dbc250dd0d63ba023d67d21ad0
Pangkeek <supawatkhaithong@gmail.com>
2024-02-13 23:04:14 +0700

    4

--- src/assets/component/page2.jsx
+++ src/assets/component/page2.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page2() {
   return (
     <div className="container mx-auto bg-neutral-900">
-        <p className="pl-170px pb-8 text-xl text-zinc-400">About</p>
+      <p className="pl-170px pb-8 text-xl text-zinc-400">About</p>
       <div className="text-white flex justify-around">
         <p className="text-white text-xl text-left">
           i were given a task of
@@ -32,6 +32,11 @@ export default function Page2() {
           <br />
           Cards.
         </p>
+      </div>
+      <div className='flex h-auto gradiant-radial'>
+        <img className='w-80 relative left-' src='.\src\assets\media\iPhone 12 Pro-1.png'/>
+        <img className='w-80 relative' src='.\src\assets\media\iPhone 12 Pro-2.png'/>
+        <img className='w-80 relative' src='.\src\assets\media\iPhone 12 Pro.png'/>
       </div>
     </div>
   );
