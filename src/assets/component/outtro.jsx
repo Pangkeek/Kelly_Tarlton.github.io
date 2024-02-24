a6413cb48e99f89dee4736514de1a13c7ae1912a
Pangkeek <supawatkhaithong@gmail.com>
2024-02-24 23:12:03 +0700

    8(1)

--- src/assets/component/outtro.jsx
+++ src/assets/component/outtro.jsx
@@ -5,7 +5,7 @@ export default function Outtro() {
     <div className='container mx-auto'>
         <p className="text-zinc-400 text-2xl pb-6 Regular pl-158px">User Journey</p>
       <video autoPlay loop muted playsInline width="full" height="auto">
-        <source src="./src/assets/media/0224(1).mp4" type="video/mp4" />
+        <source src="./src/assets/media/0224(5).mp4" type="video/mp4" />
       </video>
     </div>
   );
