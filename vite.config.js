602fe90f29ea94c9a57985300bd245ce8884fbbb
Pangkeek <supawatkhaithong@gmail.com>
2024-02-27 20:43:59 +0700

    pls

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Kelly_Tarlton.github.io/'
-});
+  build : {
+    outDir:'dist',
+  },
+  base:'/Kelly_Tarlton.github.io/'
+})
+
